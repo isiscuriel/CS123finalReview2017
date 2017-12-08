c30d21ff1f16a4ba8a9dd981b8e7ce8437534af8
Isis Curiel <curielisis16@gmail.com>
2017-12-08 15:16:22 -0600

    Create 3finalReview.java

---
+++ 3finalReview.java
@@ -0,0 +1,12 @@
+/*
+Problem 3) The body of the following method has three statements. Rewrite the body so that it 
+has just a single statement which is the simplest statement that will work.
+   private static boolean isInside(int n)
+   {
+      boolean inside = false;
+      if ( 0 < n && n < 100) inside = true;
+      return inside;
+   }
+   All you need to put is below
+    return ( 0 < n && n < 100);
+*/
